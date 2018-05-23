c456b289bb6efb5fa6e3538b3b9f4d236cf1368c
Fabio William Conceição <fabiowilliamconceicao@MacBook-Air-de-Fabio.local>
2018-05-22 23:18:44 -0300

    finished contact list

--- src/Components/Main/Contacts.js
+++ src/Components/Main/Contacts.js
@@ -1,28 +1,72 @@
 import React from 'react';
 import {
   View,
-  Text
+  Text,
+  ListView
 } from 'react-native';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
 import { userContactsFetch } from '../../Actions/AppActions';
 
+
 class Contacts extends React.Component{
+    constructor(props){
+      	super(props);
+
+    }
 
     componentWillMount() {
         this.props.userContactsFetch();
+        this.createSourceData(this.props.contacts);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.createSourceData(nextProps.contacts);
+    }
+
+    createSourceData(contacts) {
+        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        this.dataSource =  ds.cloneWithRows(contacts);
     }
 
     render() {
         return (
-            <View>
-                <Text>
-                    Contacts
-                </Text>
-            </View>
+            <ListView
+                enableEmptySections
+                dataSource={this.dataSource}
+                renderRow={data => {
+                    return (
+                      <View style={{
+                          flex: 1,
+                          padding: 20,
+                          borderBottomWidth: 1,
+                          borderColor: '#CCC'
+                        }}>
+                        <Text style={{
+                            fontSize: 25
+                          }}>
+                          {data.name}
+                        </Text>
+                        <Text style={{
+                            fontSize: 18
+                          }}>
+                          {data.email}
+                        </Text>
+                      </View>
+                    );
+                  }
+                }
+            />
         );
     }
 }
 
+mapStateToProps = state => {
+    const contacts = _.map(state.ContactListReducer, (value, id) => {
+        return { ...value, id };
+    });
+    return { contacts }
+}
 
-export default connect(null, { userContactsFetch })(Contacts)
+export default connect(mapStateToProps, { userContactsFetch })(Contacts)
--- src/Reducers/ContactListReducer.js
+++ src/Reducers/ContactListReducer.js
@@ -1,7 +1,12 @@
+import { USER_CONTACTS_LIST } from '../Actions/Types';
+
 const INITIAL_STATE = {}
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
+        case USER_CONTACTS_LIST:
+            return action.payload;
+        break;
         default:
             return state;
     }
