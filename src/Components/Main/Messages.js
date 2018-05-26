67cad01bd07245a41c237f559cf091992df3dc0f
Fabio William Conceição <fabiowilliamconceicao@MacBook-Air-de-Fabio.local>
2018-05-26 18:39:38 -0300

    refactoring messages component and page to recieve firebase chat list

--- src/Components/Main/Messages.js
+++ src/Components/Main/Messages.js
@@ -3,11 +3,36 @@ import {
   View,
   Text
 } from 'react-native';
+import { connect } from 'react-redux';
+import _ from 'lodash';
 
-export default props => (
-    <View>
-        <Text>
-            Messages
-        </Text>
-    </View>
-);
+import { chatListFetch } from '../../Actions/AppActions';
+import ChatListReducer from '../../Reducers/ChatListReducer';
+
+class Message extends React.Component {
+
+    componentWillMount() {
+        this.props.chatListFetch();
+    }
+
+    render() {
+        return (
+            <View>
+            </View>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    const chats = _.map(state.ChatListReducer, (val, uid) => {
+        return { ...val, uid };
+    });
+
+    return {
+        chats
+    }
+}
+
+export default connect(mapStateToProps, {
+    chatListFetch
+})(Message);
